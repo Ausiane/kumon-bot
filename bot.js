ad3750adc24f60172c3b5cceae1c5a758c58b1bd
Ausiane <ausiane@hotmail.com>
2026-06-02 11:10:16 -0300

    fix missing Client import render

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth({
         clientId: 'kumon-lourdes'
